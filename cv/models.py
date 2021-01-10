8e2092cc31014274122adf4ee17bc5008df9f64e
galij899 <47144051+galij899@users.noreply.github.com>
2021-01-10 23:58:13 +0300

    filling data and one-pager

--- cv/models.py
+++ cv/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Skill(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
+    level = models.IntegerField()
+    descr = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
--- cv/views.py
+++ cv/views.py
@@ -4,8 +4,12 @@ from .models import Skill
 # Create your views here.
 
 def about(request):
-    skills = Skill.objects.all()
-    return render(request, 'cv/about.html', {"skills": skills})
+    skills = Skill.objects.filter(type="Python")
+    web = Skill.objects.filter(type="Web")
+    general = Skill.objects.filter(type="General")
+    return render(request, 'cv/about.html', {"skills": skills,
+                                             "web": web,
+                                             "general": general})
 
 def skills(request):
     return render(request, 'cv/skills.html')
